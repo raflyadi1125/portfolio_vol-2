851c4f3a369cb37b734dcf1fcd243f4b778d66c1
raflyadi1125 <adirafli0112@gmail.com>
2026-09-17 17:55:08 +0700

    fix: component import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import About from "./components/About";
 import School from "./components/School";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Experience from "./components/experience";
+import Experience from "./components/Experience";
 import Certificates from "./components/Certificates";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
